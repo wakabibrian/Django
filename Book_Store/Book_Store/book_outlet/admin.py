47cdc07f9161d3c51db35afb0e382bdd5ad6e50c
Brian Wakabi <b.wakabi@corp.harissint.com>
2024-01-14 21:48:54 +0300

    Finished admin

--- Book_Store/Book_Store/book_outlet/admin.py
+++ Book_Store/Book_Store/book_outlet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)  # read only
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating", )  # field names as defined in the model
+    list_display = ("title", "author", )  # adding columns
+
+
+admin.site.register(Book, BookAdmin)

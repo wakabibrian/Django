fa61058f2259090d335cd798baeb3a84c40ebea8
Brian Wakabi <b.wakabi@corp.harissint.com>
2024-01-19 05:12:16 +0300

    Finished Relations

--- Book_Store/Book_Store/book_outlet/admin.py
+++ Book_Store/Book_Store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
@@ -12,3 +12,6 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)

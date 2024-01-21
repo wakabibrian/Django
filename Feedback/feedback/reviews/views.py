7e33b1e9aaad9d4bc99538b5100cec0b339cf2a5
Brian Wakabi <b.wakabi@corp.harissint.com>
2024-01-21 18:26:16 +0300

    Finished form views

---
+++ Feedback/feedback/reviews/views.py
@@ -0,0 +1,45 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
+from .forms import ReviewForm
+from .models import Review
+
+# Create your views here.
+
+
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/reviews_list.html"
+    model = Review
+    context_object_name = "reviews"  # Allows you to define the name of the data
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
